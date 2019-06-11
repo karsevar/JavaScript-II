648d96350bfc042f7f0d724d881be05d3eed33d8
masonkarsevar <masonkarsevar@gmail.com>
2019-06-11 12:40:05 -0700

    MVP: closure.js. WIP: not sure about the challenge 2 solution

--- assignments/closure.js
+++ assignments/closure.js
@@ -8,14 +8,48 @@
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+  return function() {
+  	return count++;
+  }
 };
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
+const newCounter = counter();
+
+// console.log(newCounter());
+// console.log(newCounter());
+// console.log(newCounter());
+// Works perfectly!!! 
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let count = 1;
+
+  return {
+  	increment: function() {
+  		return count++;
+  	},
+
+  	decrement: function() {
+  		return count--;
+  	}
+  }
 };
+
+const counterObject = counterFactory();
+console.log(counterObject.increment());
+console.log(counterObject.increment());
+console.log(counterObject.increment());
+console.log(counterObject.decrement());
+console.log(counterObject.decrement());
+console.log(counterObject.decrement());
+console.log(counterObject.increment());
+
+// The behavior seems a little weird will need to check the logic.
+
+
